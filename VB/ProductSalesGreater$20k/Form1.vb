0761c798e4431946785783c7e174e8059d02709b
DevExpressExampleBot <fakeEmail>
2020-11-17 20:36:41 +0300

    Source auto update [skip ci]

---
+++ VB/ProductSalesGreater$20k/Form1.vb
@@ -0,0 +1,21 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.Windows.Forms
+
+Namespace ProductSalesGreater_20k
+	Partial Public Class Form1
+		Inherits Form
+
+		Public Sub New()
+			InitializeComponent()
+			dashboardDesigner1.CreateRibbon()
+			dashboardDesigner1.LoadDashboard("..\..\Data\FilterByAggr_Simple.xml")
+		End Sub
+	End Class
+End Namespace
